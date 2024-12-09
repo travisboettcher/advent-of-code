84607cd9fa326fab12e76683feb76eb6da4b7c6a
Travis Boettcher <boettcherta@gmail.com>
2024-12-09 16:19:38 -0600

    Refactored day 9 solution

--- src/day_9.rs
+++ src/day_9.rs
@@ -1,5 +1,10 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead};
+use std::fmt;
+
+struct HardDrive {
+    hd: Vec<Option<usize>>
+}
 
 pub fn day_9() {
     let file = File::open("input-day9.txt").expect("no such file");
@@ -17,17 +22,17 @@ pub fn day_9() {
 }
 
 fn defrag(compact_repr: String) -> usize {
-    let mut hd = produce_hard_drive(compact_repr);
-    let mut next_file = next_file_space(&hd, hd.len());
-    let mut next_free = next_free_space(&hd, 0);
+    let mut hd = HardDrive::from_compact_representation(&compact_repr);
+    let mut next_file = hd.next_file_space(hd.hd.len());
+    let mut next_free = hd.next_free_space(0);
 
     while next_free.unwrap() < next_file.unwrap() {
-        hd.swap(next_file.unwrap(), next_free.unwrap());
-        next_file = next_file_space(&hd, next_file.unwrap());
-        next_free = next_free_space(&hd, next_free.unwrap());
+        hd.hd.swap(next_file.unwrap(), next_free.unwrap());
+        next_file = hd.next_file_space(next_file.unwrap());
+        next_free = hd.next_free_space(next_free.unwrap());
     }
     
-    hd.iter()
+    hd.hd.iter()
         .enumerate()
         .take_while(|(_, c)| c.is_some())
         .map(|(i, c)| i * c.unwrap())
@@ -35,111 +40,120 @@ fn defrag(compact_repr: String) -> usize {
 }
 
 fn defrag_2(compact_repr: String) -> usize {
-    let mut hd = produce_hard_drive(compact_repr);
-    let mut next_file = next_file_space(&hd, hd.len());
+    let mut hd = HardDrive::from_compact_representation(&compact_repr);
+    let mut next_file = hd.next_file_space(hd.hd.len());
 
     while next_file.is_some() {
-        let file_len = length_of_file(&hd, next_file.unwrap());
-        let free_space = find_free_space(&hd, file_len);
+        let file_len = hd.length_of_file(next_file.unwrap());
+        let free_space = hd.find_free_space(file_len);
 
         if free_space.is_some() && free_space.unwrap() < next_file.unwrap() {
             for i in 0..file_len {
-                hd.swap(next_file.unwrap() - i, free_space.unwrap() + i);
+                hd.hd.swap(next_file.unwrap() - i, free_space.unwrap() + i);
             }
         }
 
         if next_file.unwrap() < file_len {
             next_file = None;
         } else {
-            next_file = next_file_space(&hd, next_file.unwrap() - file_len + 1);
+            next_file = hd.next_file_space(next_file.unwrap() - file_len + 1);
         }
     }
 
-    hd.iter()
+    hd.hd.iter()
         .enumerate()
         .filter(|(_, c)| c.is_some())
         .map(|(i, c)| i * c.unwrap())
         .sum()
 }
 
-fn next_file_space(hd: &Vec<Option<usize>>, start: usize) -> Option<usize> {
-    hd.iter().enumerate()
-        .rev()
-        .skip(hd.len() - start)
-        .find(|(_, f)| f.is_some())
-        .map(|c| c.0)
-}
+impl HardDrive {
 
-fn next_free_space(hd: &Vec<Option<usize>>, start: usize) -> Option<usize> {
-    hd.iter().enumerate()
-        .skip(start)
-        .find(|(_, f)| f.is_none())
-        .map(|c| c.0)
-}
+    fn from_compact_representation(compact_repr: &str) -> HardDrive {
+        let ints: Vec<_> = compact_repr.chars()
+            .filter(|c| c.is_numeric())
+            .map(|c| c.to_string().parse::<usize>().unwrap())
+            .collect();
 
-fn length_of_file(hd: &Vec<Option<usize>>, start: usize) -> usize {
-    let find = hd.get(start).unwrap();
-    hd.iter()
-        .rev()
-        .skip(hd.len() - start - 1)
-        .take_while(|x| x == &find)
-        .count()
-}
+        let size: usize = ints.iter().sum();
 
-fn find_free_space(hd: &Vec<Option<usize>>, len: usize) -> Option<usize> {
-    let mut next_free = next_free_space(hd, 0);
-    while next_free.is_some() && !hd.iter().skip(next_free.unwrap()).take(len).all(|c| c.is_none()) {
-        next_free = next_free_space(hd, next_free.unwrap() + 1);
-    }
-
-    if hd.iter().skip(next_free.unwrap()).take(len).len() < len {
-        None
-    } else {
-        next_free
-    }
-}
-
-fn produce_hard_drive(compact_repr: String) -> Vec<Option<usize>> {
-    let ints: Vec<_> = compact_repr.chars()
-        .filter(|c| c.is_numeric())
-        .map(|c| c.to_string().parse::<usize>().unwrap())
-        .collect();
-
-    let size: usize = ints.iter().sum();
-
-    let mut hd = Vec::with_capacity(size);
-    let mut current_index = 0;
-    let mut current_file_id = 0;
-    let mut is_file = true;
-    for i in ints {
-        for _ in 0..i {
-            if is_file {
-                hd.insert(current_index, Some(current_file_id));
-            } else {
-                hd.insert(current_index, None);
+        let mut hd = Vec::with_capacity(size);
+        let mut current_index = 0;
+        let mut current_file_id = 0;
+        let mut is_file = true;
+        for i in ints {
+            for _ in 0..i {
+                if is_file {
+                    hd.insert(current_index, Some(current_file_id));
+                } else {
+                    hd.insert(current_index, None);
+                }
+                current_index += 1;
             }
-            current_index += 1;
+
+            if is_file { current_file_id += 1; }
+            is_file = !is_file;
+        }
+        
+        HardDrive { hd }
+    }
+
+
+    fn next_file_space(&self, start: usize) -> Option<usize> {
+        self.hd.iter().enumerate()
+            .rev()
+            .skip(self.hd.len() - start)
+            .find(|(_, f)| f.is_some())
+            .map(|c| c.0)
+    }
+
+    fn next_free_space(&self, start: usize) -> Option<usize> {
+        self.hd.iter().enumerate()
+            .skip(start)
+            .find(|(_, f)| f.is_none())
+            .map(|c| c.0)
+    }
+
+    fn length_of_file(&self, start: usize) -> usize {
+        let find = self.hd.get(start).unwrap();
+        self.hd.iter()
+            .rev()
+            .skip(self.hd.len() - start - 1)
+            .take_while(|x| x == &find)
+            .count()
+    }
+
+    fn find_free_space(&self, len: usize) -> Option<usize> {
+        let mut next_free = self.next_free_space(0);
+        while next_free.is_some() && !self.hd.iter().skip(next_free.unwrap()).take(len).all(|c| c.is_none()) {
+            next_free = self.next_free_space(next_free.unwrap() + 1);
         }
 
-        if is_file { current_file_id += 1; }
-        is_file = !is_file;
+        if self.hd.iter().skip(next_free.unwrap()).take(len).len() < len {
+            None
+        } else {
+            next_free
+        }
     }
-    hd
 }
 
-fn print_hd(hd: &Vec<Option<usize>>) -> String {
-    hd.iter()
-        .map(|i| {
-            if let Some(i) = i {
-                return i.to_string();
-            } else {
-                return ".".to_string();
-            }
-        })
-    .collect()
+impl fmt::Display for HardDrive {
+
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s: String = self.hd.iter()
+            .map(|i| {
+                if let Some(i) = i {
+                    return i.to_string();
+                } else {
+                    return ".".to_string();
+                }
+            })
+        .collect();
+
+        write!(f, "{s}")
+    }
+
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -149,9 +163,9 @@ mod tests {
     fn test_produce_hard_drive() {
         let compact_repr = "2333133121414131402";
 
-        let hd = produce_hard_drive(compact_repr.to_string());
+        let hd = HardDrive::from_compact_representation(&compact_repr);
         assert_eq!(
-            "00...111...2...333.44.5555.6666.777.888899", print_hd(&hd)
+            "00...111...2...333.44.5555.6666.777.888899", format!("{hd}")
         );
     }
 
